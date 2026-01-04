47ecf21f5b08854d2da732b3cbdbbd17643275c1
kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>
2026-01-04 15:52:01 +0100

    chore(ssh): update public keys
    Signed-off-by: kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>

--- modules/shared/outputs.tf
+++ modules/shared/outputs.tf
@@ -7,6 +7,6 @@ output "public_keys" {
     desktop     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4gpcZXGcMfKMZyRvLzuav955wMV7FPjdRboPKxvuLf desktop.9th-gen.ryzen"
     laptop      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtbXuVKV/wwO8q2CRsAGqftaiqOKe0NpZUnMA+Zn1+w laptop.12th-gen.intel-core"
     terraform   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtbMiGU2iLV8Zt2DnKoyqzCWGL0Q8wzfTGaDT1vnmXd terraform"
-    work-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIONZytSCpwTrg7RkQ/RnvNzMRj7zOB94NWMxXlAaYLRA laptop.200-series.ryzen"
+    work-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvMWuZwu1mAj2Ffd6nD8dc7+UtsVWHzQ9geSWMXtbTS laptop.200-series.ryzen"
   }
 }
